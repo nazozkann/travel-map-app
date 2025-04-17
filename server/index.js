5e58763f542636cbd57a9f8f09b6498411d0e447
savetheworld <goringhanne@gmail.com>
2025-04-17 18:46:09 +0300

    auth route starter added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
+app.get("/test", (req, res) => {
+  res.send("Direct test route");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
