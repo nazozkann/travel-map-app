2c61593532fe9ead36b400dca12b2af9046f8bf8
savetheworld <goringhanne@gmail.com>
2025-04-18 14:30:51 +0300

    login-register-token backend added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("../middleware/verifyToken");
 
 router.post("/register", async (req, res) => {
   console.log("➡️ /register endpoint hit");
@@ -26,9 +28,50 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  console.log("➡️ /login endpoint hit");
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "2d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error while logging in" });
+  }
+});
+
 router.get("/ping", (req, res) => {
   console.log("✅ /ping endpoint hit");
   res.json({ message: "Ping successful!" });
+});
+
+router.get("/protected", verifyToken, (req, res) => {
+  res.status(200).json({
+    message: "Protected route accessed",
+  });
 });
 
 module.exports = router;
