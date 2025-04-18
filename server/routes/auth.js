3c3c891966cd5e2f48db7a5e5366c0efcf4d5c37
savetheworld <goringhanne@gmail.com>
2025-04-18 18:17:28 +0300

    map added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,4 +29,49 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  console.log
+  console.log("➡️ /login endpoint hit");
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "2d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error while logging in" });
+  }
+});
+
+router.get("/ping", (req, res) => {
+  console.log("✅ /ping endpoint hit");
+  res.json({ message: "Ping successful!" });
+});
+
+router.get("/protected", verifyToken, (req, res) => {
+  res.status(200).json({
+    message: "Protected route accessed",
+  });
+});
+
+module.exports = router;
