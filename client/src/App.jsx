bed446c1a1d9f36cd4bffa20b064eb9a8643b9dd
savetheworld <nazozkandesign@gmail.com>
2025-05-10 21:40:10 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import BestPlaces from "./pages/BestPlaces";
 import Navbar from "./components/Navbar";
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home location={location} />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/profile/:username" element={<Profile />} />
         <Route path="/places" element={<BestPlaces />} />
         <Route path="/places/:id" element={<PinDetail />} />
